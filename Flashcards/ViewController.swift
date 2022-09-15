b5120683fede8270f4bd8fa2c355223ef89ef3dc
Yongye <103079472+algebra2boy@users.noreply.github.com>
2022-09-14 21:27:20 -0400

    Added new features

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -11,14 +11,81 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var backLabel: UILabel!
     @IBOutlet weak var frontLabel: UILabel!
+    
+    @IBOutlet weak var card: UIView!
+    
+    
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
+    
+    @IBOutlet weak var Reset: UIButton!
+    @IBOutlet weak var Congrats: UILabel!
+    
+    
+    @IBOutlet weak var ResetButton: UIButton!
+    
+    var tapped = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // set up the corner radius
+        card.layer.cornerRadius = 20.0
+        card.clipsToBounds = true
+        card.layer.shadowRadius = 50
+        card.layer.shadowOpacity = 0.8
+        card.clipsToBounds = true
+        
+        Button1.layer.cornerRadius = 20.0
+        Button1.clipsToBounds = true
+        Button1.layer.borderWidth = 3.0
+        Button1.layer.borderColor = UIColor.red.cgColor
+        
+        Button2.layer.cornerRadius = 20.0
+        Button2.clipsToBounds = true
+        Button2.layer.borderWidth = 3.0
+        Button2.layer.borderColor = UIColor.red.cgColor
+        
+        Button3.layer.cornerRadius = 20.0
+        Button3.clipsToBounds = true
+        Button3.layer.borderWidth = 3.0
+        Button3.layer.borderColor = UIColor.red.cgColor
+        
+        Congrats.isHidden = true
+        
+
     }
 
     @IBAction func disTapOnFlashcard(_ sender: Any) {
-        frontLabel.isHidden = true
+        frontLabel.isHidden = tapped
+        tapped = !tapped
     }
     
+    @IBAction func didTapOne(_ sender: Any) {
+        Button1.isHidden = true
+    }
+    
+    @IBAction func didTapTwo(_ sender: Any) {
+        frontLabel.isHidden = tapped
+        tapped = !tapped
+        Congrats.isHidden = false
+    }
+    
+    @IBAction func didTapThree(_ sender: Any) {
+        Button3.isHidden = true
+    }
+    
+    
+    @IBAction func ResetToOrgin(_ sender: Any) {
+        Congrats.isHidden = true
+        sleep(1)
+        frontLabel.isHidden = false
+        sleep(1)
+        Button1.isHidden = false
+        sleep(1)
+        Button2.isHidden = false
+        sleep(1)
+        Button3.isHidden = false
+    }
 }
 
--- README.md
+++ README.md
@@ -15,6 +15,6 @@ User can flip the card to learn more about the answer to the question.
 - [x] Build in logic to show the answer side when the card is tapped
 - [x] Push code to GitHub
 ## Optional
-- [ ] Toggle the flashcard between the question side and the answer side
-- [ ] Style the question and answer side of the card to better distinguish between the two sides
-- [ ] Add selectable multiple choice answers beneath the card
+- [x] Toggle the flashcard between the question side and the answer side
+- [x] Style the question and answer side of the card to better distinguish between the two sides
+- [x] Add selectable multiple choice answers beneath the card
